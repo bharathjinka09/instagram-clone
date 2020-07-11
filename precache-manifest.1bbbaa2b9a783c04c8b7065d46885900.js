17d652b7c977fe4147abaefa8c603144b296b35d
bharathjinka09 <bharathjinka09@gmail.com>
2020-07-11 14:22:24 +0530

    Updates

---
+++ precache-manifest.1bbbaa2b9a783c04c8b7065d46885900.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76bd656eb725d84357131c5a3694a7c7",
+    "url": "/instagram-clone/index.html"
+  },
+  {
+    "revision": "a6c3dcb67e8977e218db",
+    "url": "/instagram-clone/static/css/main.fb591924.chunk.css"
+  },
+  {
+    "revision": "96eb40b10079e56ac6ad",
+    "url": "/instagram-clone/static/js/2.a8184d25.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/instagram-clone/static/js/2.a8184d25.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a6c3dcb67e8977e218db",
+    "url": "/instagram-clone/static/js/main.f6db0d83.chunk.js"
+  },
+  {
+    "revision": "db59bf82604977057e92",
+    "url": "/instagram-clone/static/js/runtime-main.115c89fe.js"
+  }
+]);
